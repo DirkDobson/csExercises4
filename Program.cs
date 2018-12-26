5e8121397dab5f788bb7f74af146c982d7c9afec
Dirk Dobson <dirkvancouver@live.com>
2018-12-26 15:12:45 -0700

    exercise finished

--- Program.cs
+++ Program.cs
@@ -9,7 +9,20 @@ namespace csExercises4
         {
             var path = @"/Users/dirkvancouver/foobar/bar.txt";
 
-            Console.WriteLine(File.Exists(path) ? "It exists." : "It does not exist.");;
+            Console.WriteLine(File.Exists(path) ? "It exists." : "It does not exist.");
+            var file = File.ReadAllText(path);
+            var words = file.Split(" ");
+            System.Console.WriteLine(words.Length);
+            
+            var total = words[0];
+            for(var i = 0; i <= words.Length - 1; i++)
+            {
+                if(words[i].Length > total.Length)
+                {
+                    total = words[i];
+                }
+            }
+            System.Console.WriteLine(total);
         }
     }
 }
